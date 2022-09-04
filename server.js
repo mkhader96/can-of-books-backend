831be52e35a9932a988cdf641e5e77ffd00455e7
mkhader96 <mkhader996@gmail.com>
2022-09-04 20:32:27 +0300

    Modulerization

--- server.js
+++ server.js
@@ -6,47 +6,15 @@ const cors = require("cors");
 const server = express();
 const mongoose = require("mongoose");
 server.use(cors());
-
+const booksModel = require("./Modules/BookModule");
 const PORT = process.env.PORT || 3001;
 
-mongoose.connect("mongodb://localhost:27017/Books", {
+mongoose.connect("mongodb://localhost:27017/Books2", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const booksSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  status: String,
-});
 
-const booksModel = mongoose.model("books", booksSchema);
-
-async function seedBooks() {
-  const firstBooke = new booksModel({
-    title: "The Hitchhiker's Guide to the Galaxy",
-    description:
-      "The Hitchhiker's Guide to the Galaxy is a science fiction comedy series created by Douglas Adams.",
-    status: "Available",
-  });
-  const secondBooke = new booksModel({
-    title: "The Prestige",
-    description:
-      "The Prestige is a 2006 British-American mystery thriller film directed by Christopher Nolan, who co-wrote the screenplay with his brother Jonathan Nolan",
-    status: "Available",
-  });
-
-  const thirdBooke = new booksModel({
-    title: "Fight Club",
-    description:
-      "Fight Club is a 1996 novel by Chuck Palahniuk. It follows the experiences of an unnamed protagonist struggling with insomnia. Inspired by his doctor's exasperated remark that insomnia is not suffering, the protagonist finds relief by impersonating a seriously ill person in several support groups",
-    status: "Available",
-  });
-
-  await firstBooke.save();
-  await secondBooke.save();
-  await thirdBooke.save();
-}
 // seedBooks();
 
 server.get("/", homeHandler);
